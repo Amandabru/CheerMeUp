fde6f7836bb3887cd0157ee9bbbe23867e3dd264
Frida J <97102212+frjda@users.noreply.github.com>
2023-09-20 10:00:54 +0200

    Add first test version of nav-bar with daisy UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,19 +1,21 @@
-import './App.css';
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { deleteDeck } from './api/deleteDeck';
-import { TDeck, getDecks } from './api/getDecks';
-import { createDeck } from './api/createDeck';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+import { deleteDeck } from "./api/deleteDeck";
+import { TDeck, getDecks } from "./api/getDecks";
+import { createDeck } from "./api/createDeck";
+
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   const [decks, setDecks] = useState<TDeck[]>([]);
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
 
   async function handleCreateDeck(e: React.FormEvent) {
     e.preventDefault();
     const deck = await createDeck(title);
     setDecks([...decks, deck]);
-    setTitle('');
+    setTitle("");
   }
 
   async function handleDeleteDeck(deckId: string) {
@@ -30,10 +32,12 @@ function App() {
   }, []);
 
   return (
-    <div className='container'>
-      <div className='App'>
+    <div className="container">
+      <NavigationBar />
+
+      <div className="App">
         <h1>Your Decks</h1>
-        <ul className='decks'>
+        <ul className="decks">
           {decks.map((deck) => (
             <li key={deck._id}>
               <button onClick={() => handleDeleteDeck(deck._id)}>X</button>
@@ -42,9 +46,9 @@ function App() {
           ))}
         </ul>
         <form onSubmit={handleCreateDeck}>
-          <label htmlFor='deck-title'>Deck Title</label>
+          <label htmlFor="deck-title">Deck Title</label>
           <input
-            id='deck-title'
+            id="deck-title"
             value={title}
             onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
               setTitle(e.target.value);
---
+++ client/src/components/NavigationBar.tsx
@@ -0,0 +1,15 @@
+// import { Link } from "react-router-dom";
+
+function NavigationBar() {
+  return (
+    <div className="navbar bg-base-100">
+      <a className="btn btn-ghost normal-case text-xl">CheerMeUp!</a>
+      <a className="btn btn-ghost normal-case text-xl">News</a>
+      <a className="btn btn-ghost normal-case text-xl">Memes</a>
+      <a className="btn btn-ghost normal-case text-xl">Jokes</a>
+      <a className="btn btn-ghost normal-case text-xl">Suggestions</a>
+    </div>
+  );
+}
+
+export default NavigationBar;
