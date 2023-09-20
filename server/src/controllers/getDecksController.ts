562fdac4084277115f34d9e73f42c1daa93b80f4
Amanda Brundin <abrundin@kth.se>
2023-09-20 14:29:49 +0200

    Add fetch to news api

--- server/src/controllers/getDecksController.ts
+++ server/src/controllers/getDecksController.ts
@@ -1,11 +1,8 @@
 import { Request, Response } from 'express';
 import Deck from '../models/Deck';
+import fetch from 'node-fetch';
 
 export async function getDecksController(req: Request, res: Response) {
   const decks = await Deck.find();
-  const api_url = "https://api.worldnewsapi.com/search-news?text=tesla&api-key=d5c93120058d4ee88131ab766649f41c";
-  const response = await fetch(api_url);
-  const json = await response.json();
-  console.log(json);
   res.json(decks);
 }
