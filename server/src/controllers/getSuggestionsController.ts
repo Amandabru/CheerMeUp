64bc65aea3b782b2a27a2f17a3d94a26319f794a
Amanda Brundin <abrundin@kth.se>
2023-09-20 14:41:40 +0200

    create suggestionController

---
+++ server/src/controllers/getSuggestionsController.ts
@@ -0,0 +1,7 @@
+import { Request, Response } from 'express';
+
+export async function getSuggestionsController(req: Request, res: Response) {
+    // const type = req.type;
+    const api_url = 
+    //res.json(decks);
+}
