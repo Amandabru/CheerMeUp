8b4ff13fb17b74e657724852cbefd605a0003416
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-02 10:47:22 +0200

    Add daisyUI loading GIF

--- client/src/PromiseNoData.tsx
+++ client/src/PromiseNoData.tsx
@@ -1,18 +1,18 @@
 function promiseNoData(
-  promise: Promise<string | null>,
-  data: string | object | Error,
-  error: Error | null,
-  noDataMessage: string
+    promise: Promise<string | null>,
+    data: string | object | Error,
+    error: Error | null,
+    noDataMessage: string
 ) {
-  if (!promise) {
-    return <span> {noDataMessage} </span>;
-  } else if (!data && !error) {
-    return <img src="http://www.csc.kth.se/~cristi/loading.gif"></img>;
-  } else if (!data && error) {
-    return <span> {error.message} </span>;
-  }
+    if (!promise) {
+        return <span> {noDataMessage} </span>;
+    } else if (!data && !error) {
+        return <span className="loading loading-dots loading-md"></span>;
+    } else if (!data && error) {
+        return <span> {error.message} </span>;
+    }
 
-  return false;
+    return false;
 }
 
 export default promiseNoData;
