5d9dbdf726757851089e7d614b035fa124917412
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-02 11:01:26 +0200

    Convert login components to presenters and views

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,71 +5,71 @@ import JokeView from './pages/jokes/JokeView';
 import MemeView from './pages/memes/MemeView';
 import NewsView from './pages/news/NewsView';
 import NotFoundView from './pages/NotFoundView';
-import NavigationBar from './components/NavigationBar';
+import NavBarPresenter from './components/NavBar/NavBarPresenter';
 import { useState, useEffect } from 'react';
 import { User } from './userModel';
 import * as userApi from './api/user';
-import SignUpModal from './components/SignUpModal';
-import LoginModal from './components/LoginModal';
+import LoginPresenter from './components/Login/LoginPresenter';
+import SignUpPresenter from './components/SignUp/SignUpPresenter';
 
 function App() {
-  const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
+    const [loggedInUser, setLoggedInUser] = useState<User | null>(null);
 
-  function closeModal(modalId: string) {
-    if (document) {
-      (document.getElementById(modalId) as HTMLFormElement).close();
+    function closeModal(modalId: string) {
+        if (document) {
+            (document.getElementById(modalId) as HTMLFormElement).close();
+        }
     }
-  }
 
-  function showModal(modalId: string) {
-    if (document) {
-      (document.getElementById(modalId) as HTMLFormElement).showModal();
+    function showModal(modalId: string) {
+        if (document) {
+            (document.getElementById(modalId) as HTMLFormElement).showModal();
+        }
     }
-  }
 
-  useEffect(() => {
-    async function fetchLoggedInUser() {
-      try {
-        const user = await userApi.getLoggedInUser();
-        setLoggedInUser(user);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchLoggedInUser();
-  }, []);
+    useEffect(() => {
+        async function fetchLoggedInUser() {
+            try {
+                const user = await userApi.getLoggedInUser();
+                setLoggedInUser(user);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchLoggedInUser();
+    }, []);
 
-  return (
-    <>
-      <NavigationBar
-        loggedInUser={loggedInUser}
-        onLoginClicked={() => showModal('login_modal')}
-        onSignUpClicked={() => showModal('signup_modal')}
-        onLogoutSuccessful={() => setLoggedInUser(null)}
-      />
-      <div>
-        <Routes>
-          <Route path='/' element={<HomeView />} />
-          <Route path='/jokes' element={<JokeView />} />
-          <Route path='/memes' element={<MemeView />} />
-          <Route path='/news' element={<NewsView />} />
-          <Route path='/*' element={<NotFoundView />} />
-        </Routes>
-      </div>
-      <SignUpModal
-        onSignUpSuccessful={(user) => {
-          setLoggedInUser(user);
-          closeModal('signup_modal');
-        }}
-      />
-      <LoginModal
-        onLoginSuccessful={(user) => {
-          setLoggedInUser(user);
-          closeModal('login_modal');
-        }}
-      />
-    </>
-  );
+    return (
+        <>
+            <NavBarPresenter
+                loggedInUser={loggedInUser}
+                onLoginClicked={() => showModal('login_modal')}
+                onSignUpClicked={() => showModal('signup_modal')}
+                onLogoutSuccessful={() => setLoggedInUser(null)}
+            />
+            <div>
+                <Routes>
+                    <Route path="/" element={<HomeView />} />
+                    <Route path="/jokes" element={<JokeView />} />
+                    <Route path="/memes" element={<MemeView />} />
+                    <Route path="/news" element={<NewsView />} />
+                    <Route path="/*" element={<NotFoundView />} />
+                </Routes>
+            </div>
+            <SignUpPresenter
+                onSignUpSuccessful={(user) => {
+                    setLoggedInUser(user);
+                    closeModal('signup_modal');
+                }}
+            />
+            <LoginPresenter
+                onLoginSuccessful={(user) => {
+                    setLoggedInUser(user);
+                    closeModal('login_modal');
+                }}
+            />
+        </>
+    );
 }
 
 export default App;
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/*.{js,ts,jsx,tsx}",
-    "./src/components/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require("daisyui")],
+    content: [
+        './index.html',
+        './src/*.{js,ts,jsx,tsx}',
+        './src/components/*/*.{js,ts,jsx,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    plugins: [require('daisyui')]
 };
