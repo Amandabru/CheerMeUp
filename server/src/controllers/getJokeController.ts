8824e4889010731b70a5289162f796fdb5ddabbb
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-02 22:00:49 +0200

    Update getJokeController.ts

--- server/src/controllers/getJokeController.ts
+++ server/src/controllers/getJokeController.ts
@@ -18,7 +18,7 @@ export async function getJokeController(
 
         /* External Jokes API response errors are stored in boolean error parameter*/
         if (data.error) {
-            throw createHttpError(data.code, data.message);
+            throw createHttpError(500, data.message);
         }
         const selectedData = {
             type: 'joke',
