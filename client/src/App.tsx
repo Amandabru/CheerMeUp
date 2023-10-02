6debfbb507441fc126f13e41c106ea400a12e0a6
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-02 10:47:00 +0200

    Update routes + nav

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -8,8 +8,10 @@ import NotFoundView from './pages/NotFoundView';
 import NavigationBar from './components/NavigationBar';
 
 import { CheerModel } from './models/model';
+import SuggestionPresenter from './pages/suggestions/SuggestionPresenter';
 
 function App() {
+    const model = new CheerModel();
     return (
         <>
             <NavigationBar />
@@ -19,6 +21,10 @@ function App() {
                     <Route path="/jokes" element={<JokeView />} />
                     <Route path="/memes" element={<MemeView />} />
                     <Route path="/news" element={<NewsView />} />
+                    <Route
+                        path="/suggestions"
+                        element={<SuggestionPresenter model={model} />}
+                    />
                     <Route path="/*" element={<NotFoundView />} />
                 </Routes>
             </div>
