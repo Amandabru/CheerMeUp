63da97f6c973cbb2442bd381717d69569706674f
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-03 14:32:50 +0200

    Heart is hidden before search is done

--- client/src/pages/jokes/JokeView.tsx
+++ client/src/pages/jokes/JokeView.tsx
@@ -1,4 +1,5 @@
 import HeartIcon from '../../components/UI/HeartIcon';
+import { useState } from 'react';
 
 function JokeView({
     randomJoke,
@@ -13,6 +14,10 @@ function JokeView({
     liked: boolean;
     isLiked: Function;
 }) {
+    const [hidden, setVisability] = useState<
+        'visible' | 'hidden' | 'collapse' | undefined
+    >('hidden');
+
     return (
         <div className="bg-lime-200	text-black h-full w-full fixed">
             <div className="absolute top-[40%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-1/2 text-center">
@@ -54,10 +59,12 @@ function JokeView({
                 </div>
                 <div className="m-auto p-10 text-center border-2 border-solid border-white rounded-2xl bg-lime-100 h-40 w-full overflow-x-auto flex items-center justify-center relative">
                     <span
+                        // TODO: Implement with model
                         onClick={() => {
                             liked = !liked;
                             isLiked(liked);
                         }}
+                        style={{ visibility: hidden }}
                     >
                         <HeartIcon
                             isSolid={liked} // likedList.find((likedObject => likedObject.id === liked.id))
@@ -75,8 +82,9 @@ function JokeView({
                 <button
                     className="btn mt-5 transition-transform min-w-fit"
                     onClick={() => {
-                        if (jokeType != null) {
+                        if (jokeType != null && hidden != 'visible') {
                             onNewJoke([jokeType]);
+                            setVisability('visible');
                         }
                     }}
                 >
