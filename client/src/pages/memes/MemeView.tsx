d25c28ac5edd5435ae03227d80d450e2a1e18fb1
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-04 16:21:48 +0200

    add functionality buttons

--- client/src/pages/memes/MemeView.tsx
+++ client/src/pages/memes/MemeView.tsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
 import RandomMemeView from './RandomMemeView';
 import BestMemeView from './BestMemeView';
 
 function MemeView() {
+    const [selectedView, setSelectedView] = useState<'random' | 'best'>(
+        'random'
+    );
+    const renderSelectedView = () => {
+        if (selectedView === 'random') {
+            return <RandomMemeView />;
+        } else if (selectedView === 'best') {
+            return <BestMemeView />;
+        }
+    };
+
     return (
-        <body className="bg-blue-300 text-black h-full w-full fixed">
+        <div className="bg-blue-300 text-black h-full w-full fixed">
             <div>
-                <h1>Meme page in progress</h1>
-                <div className="flex align-middle">
-                    <button className="btn btn-accent">Random</button>
-                    <button className="btn btn-accent">Best</button>
+                <div className="flex justify-center items-center">
+                    <button
+                        className="btn btn-accent mr-5 mt-10"
+                        onClick={() => setSelectedView('random')}
+                    >
+                        Random
+                    </button>
+                    <button
+                        className="btn btn-accent ml-5 mt-10"
+                        onClick={() => setSelectedView('best')}
+                    >
+                        Best
+                    </button>
                 </div>
             </div>
-        </body>
+            {renderSelectedView()}
+        </div>
     );
 }
 
--- client/src/pages/memes/RandomMemeView.tsx
+++ client/src/pages/memes/RandomMemeView.tsx
@@ -4,7 +4,6 @@ function RandomMemeView() {
     return (
         <body className="bg-blue-300 text-black h-full w-full fixed">
             <div>
-                <h1>Meme page in progress</h1>
                 <section className="m-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
                     {/* First Card*/}
                     <Card image="/src/images/shoe.jpeg" />
