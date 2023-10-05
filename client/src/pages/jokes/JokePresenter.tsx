4c7ef0e333ef6d573c6d0dd1c1b129c5d74fbc09
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-05 11:03:26 +0200

    Add sound functions

--- client/src/pages/jokes/JokePresenter.tsx
+++ client/src/pages/jokes/JokePresenter.tsx
@@ -12,7 +12,18 @@ function JokePresenter({ model }: { model: CheerModel }) {
     // TODO: Implement with model
 
     const [liked, isLiked] = useState<boolean>(false);
+    let santaLaugh = new Audio("../../assets/audio/santa.mp3");
+    let spookyLaugh = new Audio("../../assets/audio/spooky.mp3");
 
+    const playSantaLaugh = () => {
+        santaLaugh.play()
+      }
+
+      const playspookyLaugh = () => {
+        spookyLaugh.play()
+      }
+
+    
     return (
         <JokeView
             randomJoke={
