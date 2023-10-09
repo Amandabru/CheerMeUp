8ac9a8baa57a3e25d8472d9e46be4b87290f9ab8
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-09 16:09:17 +0200

    update getMemes.ts
    add cacheBuster

--- client/src/api/getMemes.ts
+++ client/src/api/getMemes.ts
@@ -1,7 +1,20 @@
-import { API_URL } from './config';
+/*import { API_URL } from './config';
 import { MemeType } from '../Types';
 
 export async function getMemes(): Promise<MemeType[]> {
     const response = await fetch(`${API_URL}/memes`);
+    return response.json();
+}*/
+import { API_URL } from './config';
+import { MemeType } from '../Types';
+
+export async function getMemes(): Promise<MemeType[]> {
+    // Generate a random string to append as a query parameter
+    const cacheBuster = Math.random().toString(36).substring(7);
+
+    // Append the cache buster as a query parameter to the URL
+    const urlWithCacheBuster = `${API_URL}/memes?cacheBuster=${cacheBuster}`;
+
+    const response = await fetch(urlWithCacheBuster);
     return response.json();
 }
