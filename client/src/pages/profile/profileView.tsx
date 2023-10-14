816516ae44f2e18b0cc2ab21231865625e467c7b
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-14 09:17:14 +0200

    Revert "update profileView message"
    This reverts commit 7706ad8037af5e7801445544d531619a6c67e8b8.

--- client/src/pages/profile/profileView.tsx
+++ client/src/pages/profile/profileView.tsx
@@ -1,22 +1,15 @@
 import { User } from '../../userModel';
 import { getLikedJoys } from '../../api/user';
-import { CheerModel } from '../../models/model';
 
-function ProfileView({
-    model,
-    user,
-    directToLogin
-}: {
-    model: CheerModel;
-    user: User | null;
-    directToLogin: Function;
-}) {
+interface ProfileViewProps {
+    loggedInUser: User | null;
+}
+
+function ProfileView({ loggedInUser }: ProfileViewProps) {
+    const user = loggedInUser;
     return (
         <div className="bg-red-300 text-black min-h-screen bg-fixed">
-            <div className="flex justify-center text-3xl font-bold">
-                <h1 className="mt-10">Hi {user?.username}</h1>
-                <h1 className="mt-10">liked joys in progress</h1>
-            </div>
+            <h1>Hi {user?.username}</h1>
         </div>
     );
 }
