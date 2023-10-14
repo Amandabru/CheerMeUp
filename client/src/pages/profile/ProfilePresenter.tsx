06d9b2f290481c6dd1196e957b0406d0b3b4da67
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-14 20:49:45 +0200

    add liked memes to profile

--- client/src/pages/profile/ProfilePresenter.tsx
+++ client/src/pages/profile/ProfilePresenter.tsx
@@ -1,15 +1,21 @@
 import ProfileView from './ProfileView';
 import { CheerModel } from '../../models/model';
 import { User } from '../../userModel';
+import promiseNoData from '../../PromiseNoData';
+import useModelProp from '../../hooks/useModelProp';
+import { useState, useEffect } from 'react';
+import { MemeType } from '../../Types';
 
-function ProfilePresenter({
-    model,
-    user
-}: {
-    model: CheerModel;
-    user: User | null;
-}) {
-    return <ProfileView loggedInUser={user}></ProfileView>;
+function ProfilePresenter({ model, user }: { model: CheerModel; user: User }) {
+    const likedJoys = useModelProp(model, 'likedJoys');
+
+    return (
+        <ProfileView
+            loggedInUser={user}
+            likedMemes={likedJoys.memes}
+            likePost={(meme: MemeType) => model.likeOrUnlikeMeme(meme)}
+        ></ProfileView>
+    );
 }
 
 export default ProfilePresenter;
--- client/src/pages/profile/profileView.tsx
+++ client/src/pages/profile/profileView.tsx
@@ -1,17 +1,48 @@
 import { User } from '../../userModel';
 import { getLikedJoys } from '../../api/user';
+import { MemeType } from '../../Types';
+import Card from '../../components/Card';
 
-interface ProfileViewProps {
-    loggedInUser: User | null;
-}
-
-function ProfileView({ loggedInUser }: ProfileViewProps) {
+function ProfileView({
+    loggedInUser,
+    likedMemes,
+    likePost
+}: {
+    loggedInUser: User;
+    likedMemes: MemeType[];
+    likePost: Function;
+}) {
     const user = loggedInUser;
     return (
         <div className="bg-red-300 text-black min-h-screen bg-fixed">
             <div className="flex justify-center text-3xl font-bold">
                 <h1 className="mt-10">Hi {user?.username}</h1>
             </div>
+            <section className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-20 ml-40 mr-40 mt-20">
+                {likedMemes ? (
+                    likedMemes.map((memeObject, index) => {
+                        return (
+                            <Card
+                                type="meme"
+                                key={index}
+                                image={memeObject.url}
+                                isLiked={
+                                    likedMemes.find(
+                                        (meme) => meme.url === memeObject.url
+                                    )
+                                        ? true
+                                        : false
+                                }
+                                handleLike={() => {
+                                    likePost(memeObject);
+                                }}
+                            ></Card>
+                        );
+                    })
+                ) : (
+                    <div>No meme data available</div>
+                )}
+            </section>
         </div>
     );
 }
