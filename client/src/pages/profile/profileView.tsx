7706ad8037af5e7801445544d531619a6c67e8b8
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-14 09:12:30 +0200

    update profileView message

--- client/src/pages/profile/profileView.tsx
+++ client/src/pages/profile/profileView.tsx
@@ -1,15 +1,22 @@
 import { User } from '../../userModel';
 import { getLikedJoys } from '../../api/user';
+import { CheerModel } from '../../models/model';
 
-interface ProfileViewProps {
-    loggedInUser: User | null;
-}
-
-function ProfileView({ loggedInUser }: ProfileViewProps) {
-    const user = loggedInUser;
+function ProfileView({
+    model,
+    user,
+    directToLogin
+}: {
+    model: CheerModel;
+    user: User | null;
+    directToLogin: Function;
+}) {
     return (
         <div className="bg-red-300 text-black min-h-screen bg-fixed">
-            <h1>Hi {user?.username}</h1>
+            <div className="flex justify-center text-3xl font-bold">
+                <h1 className="mt-10">Hi {user?.username}</h1>
+                <h1 className="mt-10">liked joys in progress</h1>
+            </div>
         </div>
     );
 }
