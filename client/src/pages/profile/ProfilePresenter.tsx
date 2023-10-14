dcaeb729b0129a1d914dcb50809d756c3b25f51f
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-14 23:04:15 +0200

    add button to direct if no liked joys

--- client/src/pages/profile/ProfilePresenter.tsx
+++ client/src/pages/profile/ProfilePresenter.tsx
@@ -4,16 +4,38 @@ import { User } from '../../userModel';
 import promiseNoData from '../../PromiseNoData';
 import useModelProp from '../../hooks/useModelProp';
 import { useState, useEffect } from 'react';
-import { DataStructure, MemeType } from '../../Types';
+import { DataStructure, MemeType, NewsType, JokeType } from '../../Types';
 
 function ProfilePresenter({ model, user }: { model: CheerModel; user: User }) {
     const likedJoys: DataStructure = useModelProp(model, 'likedJoys');
 
+    // Function to shuffle an array (Fisher-Yates shuffle)
+    const shuffleArray = (array: (MemeType | NewsType | JokeType)[]) => {
+        for (let i = array.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [array[i], array[j]] = [array[j], array[i]];
+        }
+    };
+
+    // Combine all liked items into a single array (not used becuse it did not work in ProfileView)
+    const allLikedItems = [
+        ...likedJoys.memes,
+        ...likedJoys.news,
+        ...likedJoys.jokes
+    ];
+
+    shuffleArray(allLikedItems);
+
     return (
         <ProfileView
             loggedInUser={user}
+            likedJoys={allLikedItems}
             likedMemes={likedJoys.memes}
-            likePost={(meme: MemeType) => model.likeOrUnlikeMeme(meme)}
+            likedNews={likedJoys.news}
+            likedJokes={likedJoys.jokes}
+            likeMemePost={(meme: MemeType) => model.likeOrUnlikeMeme(meme)}
+            likeNewsPost={(news: NewsType) => model.likeOrUnlikeNews(news)}
+            likeJokePost={(joke: JokeType) => model.likeOrUnlikeJoke(joke)}
         ></ProfileView>
     );
 }
--- client/src/pages/profile/profileView.tsx
+++ client/src/pages/profile/profileView.tsx
@@ -1,25 +1,46 @@
 import { User } from '../../userModel';
-import { getLikedJoys } from '../../api/user';
-import { MemeType } from '../../Types';
+import { MemeType, NewsType, JokeType } from '../../Types';
 import Card from '../../components/Card';
+import { SadSmiley } from '../../components/UI/Icons';
+import { Link } from 'react-router-dom';
+
+function SectionHeader(title: string) {
+    return (
+        <div className="flex justify-center text-2xl font-bold mb-4">
+            {title}
+        </div>
+    );
+}
 
 function ProfileView({
     loggedInUser,
+    likedJoys,
     likedMemes,
-    likePost
+    likedNews,
+    likedJokes,
+    likeMemePost,
+    likeNewsPost,
+    likeJokePost
 }: {
     loggedInUser: User;
+    likedJoys: (MemeType | NewsType | JokeType)[];
     likedMemes: MemeType[];
-    likePost: Function;
+    likedNews: NewsType[];
+    likedJokes: JokeType[];
+    likeMemePost: Function;
+    likeNewsPost: Function;
+    likeJokePost: Function;
 }) {
     const user = loggedInUser;
+
     return (
         <div className="bg-red-300 text-black min-h-screen bg-fixed">
             <div className="flex justify-center text-3xl font-bold">
-                <h1 className="mt-10">Hi {user?.username}</h1>
+                <h1 className="my-10">Hi {user?.username}</h1>
             </div>
+            {SectionHeader('Memes')}
             <section className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-20 ml-40 mr-40 mt-20">
-                {likedMemes ? (
+                {likedMemes.length > 0 ? (
                     likedMemes.map((memeObject, index) => {
                         return (
                             <Card
@@ -34,13 +55,92 @@ function ProfileView({
                                         : false
                                 }
                                 handleLike={() => {
-                                    likePost(memeObject);
+                                    likeMemePost(memeObject);
                                 }}
                             ></Card>
                         );
                     })
                 ) : (
-                    <div>No meme data available</div>
+                    <>
+                        <div>You have no liked memes {SadSmiley()}</div>
+                        <Link to="/memes">
+                            <button className="btn btn-accent mt-10">
+                                Show me memes!
+                            </button>
+                        </Link>
+                    </>
+                )}{' '}
+            </section>
+            {SectionHeader('News')}
+            <section className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-20 ml-40 mr-40 mt-20">
+                {likedNews.length > 0 ? (
+                    likedNews.map((newsObject, index) => {
+                        return (
+                            <Card
+                                type="news"
+                                key={index}
+                                image={newsObject.urlToImage}
+                                title={newsObject.title}
+                                text={newsObject.text}
+                                author={newsObject.author}
+                                published={newsObject.publishedAt}
+                                source={newsObject.source}
+                                url={newsObject.url}
+                                isLiked={
+                                    likedNews.find(
+                                        (news) => news.url === newsObject.url
+                                    )
+                                        ? true
+                                        : false
+                                }
+                                handleLike={() => {
+                                    likeNewsPost(newsObject);
+                                }}
+                            ></Card>
+                        );
+                    })
+                ) : (
+                    <>
+                        <div>You have no liked news {SadSmiley()}</div>
+                        <Link to="/news">
+                            <button className="btn btn-accent mt-10">
+                                Show me happy news!
+                            </button>
+                        </Link>
+                    </>
+                )}
+            </section>
+            {SectionHeader('Jokes')}
+            <section className="grid grid-cols-1 md:grid-cols-2 place-items-center gap-20 ml-40 mr-40 mt-20">
+                {likedJokes.length > 0 ? (
+                    likedJokes.map((jokeObject, index) => {
+                        return (
+                            <Card
+                                type="joke"
+                                key={index}
+                                text={jokeObject.text}
+                                isLiked={
+                                    likedJokes.find(
+                                        (joke) => joke.text === jokeObject.text
+                                    )
+                                        ? true
+                                        : false
+                                }
+                                handleLike={() => {
+                                    likeJokePost(jokeObject);
+                                }}
+                            ></Card>
+                        );
+                    })
+                ) : (
+                    <>
+                        <div>You have no liked jokes {SadSmiley()}</div>
+                        <Link to="/jokes">
+                            <button className="btn btn-accent mt-10">
+                                Show me jokes!
+                            </button>
+                        </Link>
+                    </>
                 )}
             </section>
         </div>
