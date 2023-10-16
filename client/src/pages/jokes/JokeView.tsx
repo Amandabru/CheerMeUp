6ab502e3eaa6645ec752bea42fba6b8b78de6ef8
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-16 13:38:09 +0200

    Update color

--- client/src/pages/jokes/JokeView.tsx
+++ client/src/pages/jokes/JokeView.tsx
@@ -84,7 +84,7 @@ function JokeView({
                 </div>
                 <div
                     className="m-auto p-10 text-center border-2 border-solid border-white rounded-2xl bg-green-100 h-40 w-full overflow-x-auto flex items-center justify-center relative 
-                    dark:bg-[#1d401d] dark:text-gray-300 dark:border-[#31572C]"
+                    dark:bg-[#183618] dark:text-gray-300 dark:border-[#1f471f]"
                 >
                     <span
                         onClick={() => {
