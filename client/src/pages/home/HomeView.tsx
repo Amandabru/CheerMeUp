a0789d15af86ad91afe150657f224be88b258253
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-16 06:10:17 +0200

    update styling HomeView

--- client/src/pages/home/HomeView.tsx
+++ client/src/pages/home/HomeView.tsx
@@ -48,16 +48,16 @@ function HomeView({
             <h1 className=" absolute top-[20%] text-4xl font-bold left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-144">
                 Cheer Me Up!
             </h1>
-            <h2 className="absolute top-[26%] text-2xl font-light left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-144">
+            <h2 className="absolute top-[32%] text-2xl font-light left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-144">
                 Welcome to the hub of happiness! If you're feeling bored, in
                 need of a good laugh, or have temporarily lost faith in
                 humanity, you're in the right place. Our mission is simple: to
                 brighten your day!
             </h2>
-            <div className="absolute top-[28%] bg-orange-300 ">
-                <div className="flex justify-center items-center !scroll-smooth">
+            <div className="absolute top-[42%] bg-pink-300">
+                <div className="flex justify-center  !scroll-smooth">
                     <button
-                        className={`btn btn-accent mb-2 ${
+                        className={`btn btn-accent mb-2 mr-10${
                             selectedView === 'recently' ? 'btn-active' : ''
                         }`}
                         onClick={() => setSelectedView('recently')}
