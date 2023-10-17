07453a705d702ad24303256f57d31d24d3565e01
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-17 15:40:10 +0200

    Update usePromise.ts

--- client/src/hooks/usePromise.ts
+++ client/src/hooks/usePromise.ts
@@ -1,10 +1,10 @@
 import { useEffect, useState } from 'react';
-import { ActivityType } from '../Types';
+import { ActivityType, JokeType } from '../Types';
 
-// TODO: any
-
-function usePromise<T>(promise: Promise<T> | null): any {
-    const [data, setData] = useState<ActivityType | null>(null);
+function usePromise(
+    promise: Promise<ActivityType | JokeType | null> | null
+): any {
+    const [data, setData] = useState<ActivityType | JokeType | null>(null);
     const [error, setError] = useState<Error | null>(null);
     useEffect(
         function () {
@@ -13,10 +13,10 @@ function usePromise<T>(promise: Promise<T> | null): any {
             let cancelled = false;
             if (promise)
                 promise
-                    .then((dt: any) => {
+                    .then((dt: ActivityType | JokeType | null) => {
                         if (!cancelled) setData(dt);
                     })
-                    .catch((er: any) => {
+                    .catch((er: Error | null) => {
                         if (!cancelled) setError(er);
                     });
             return function () {
