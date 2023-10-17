1470e31c58e64643343dafc4c7f992872396dbcb
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-17 13:49:58 +0200

    Add background to promise no data

--- client/src/PromiseNoData.tsx
+++ client/src/PromiseNoData.tsx
@@ -35,7 +35,9 @@ function promiseNoData(
         !error
     ) {
         return (
-            <span className="loading loading-dots loading-md top-1/2 left-1/2 absolute"></span>
+            <div className="min-h-screen min-w-screen bg-black">
+                <span className="loading loading-dots loading-md top-1/2 left-1/2 absolute"></span>
+            </div>
         );
     } else if (!data && error) {
         return <span> {error.message} </span>;
