8f67a7a157d448141d4b87a9605cf9caf6570284
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-20 16:52:35 +0200

    Revert "add useMemo to list of activities"
    This reverts commit 834eac0c585e8acd720c01737b38b17e95994df7.

--- client/src/pages/activities/ActivityPresenter.tsx
+++ client/src/pages/activities/ActivityPresenter.tsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from 'react';
+import { useState } from 'react';
 import ActivityView from './ActivityView';
 import promiseNoData from '../../PromiseNoData';
 import { getActivity } from '../../api/getActivity';
@@ -10,20 +10,20 @@ function ActivityPresenter() {
     const [data, error] = usePromise(promise);
     const [activityType, setActivityType] = useState<string>('');
 
-    const options = useMemo(
-        () => [
-            { value: 'education', label: 'education' },
-            { value: 'recreational', label: 'recreational' },
-            { value: 'social', label: 'social' },
-            { value: 'diy', label: 'diy' },
-            { value: 'charity', label: 'charity' },
-            { value: 'cooking', label: 'cooking' },
-            { value: 'relaxation', label: 'relaxation' },
-            { value: 'charity', label: 'charity' },
-            { value: 'busywork', label: 'busywork' }
-        ],
-        []
-    );
+    let options: {
+        value: string;
+        label: string;
+    }[] = [
+        { value: 'education', label: 'education' },
+        { value: 'recreational', label: 'recreational' },
+        { value: 'social', label: 'social' },
+        { value: 'diy', label: 'diy' },
+        { value: 'charity', label: 'charity' },
+        { value: 'cooking', label: 'cooking' },
+        { value: 'relaxation', label: 'relaxation' },
+        { value: 'charity', label: 'charity' },
+        { value: 'busywork', label: 'busywork' }
+    ];
 
     const getRandomActivity = async (newActivityType: string) => {
         setActivityType(newActivityType);
