1fb52257e576da24dba4b055fe1bb90994259962
Hanna Snarberg <hannasnaris@gmail.com>
2023-10-21 16:05:37 +0200

    Persistance works

--- client/src/models/model.ts
+++ client/src/models/model.ts
@@ -1,13 +1,9 @@
-import { patchLike } from '../api/patchLike';
-import { getJoy } from '../api/getJoy';
-import { postLike } from '../api/postLike';
-import { JoyToUpdateType } from '../Types';
 import { DataStructure, MemeType, JokeType, NewsType } from '../Types';
 
 export class CheerModel {
     private observers: (() => void)[];
     public likedJoys: DataStructure;
-    public currentMeme: MemeType | null;
+    public currentJoy: MemeType | NewsType | JokeType | null;
 
     constructor(
         observers = [],
@@ -17,115 +13,76 @@ export class CheerModel {
             memes: [],
             news: []
         },
-        currentMeme = null
+        currentJoy = null
     ) {
         this.observers = observers;
         this.likedJoys = likedJoys;
-        this.currentMeme = currentMeme;
+        this.currentJoy = currentJoy;
     }
 
     getLikedJoys(): DataStructure {
         return { ...this.likedJoys };
     }
 
-    getCurrentMeme(): MemeType | null {
-        return this.currentMeme;
+    getCurrentJoy(): MemeType | JokeType | NewsType | null {
+        return this.currentJoy;
     }
 
-    resetCurrentMeme() {
-        this.currentMeme = null;
+    resetCurrentJoy() {
+        this.currentJoy = null;
     }
 
     setLikedJoys(likedJoys: DataStructure | undefined) {
         if (likedJoys) {
             this.likedJoys = { ...likedJoys };
-            this.currentMeme = null;
+            this.currentJoy = null;
         }
         this.notifyObservers();
     }
 
     async likeOrUnlikeMeme(likedMeme: MemeType) {
-        try {
-            const likedJoysCopy = { ...this.likedJoys };
-            const index = likedJoysCopy.memes.findIndex(
-                (meme) => meme.url === likedMeme.url
-            );
-
-            if (index !== -1) {
-                likedJoysCopy.memes.splice(index, 1);
-            } else {
-                likedJoysCopy.memes.push(likedMeme);
-            }
-            this.setLikedJoys(likedJoysCopy);
-            this.currentMeme = likedMeme;
-            this.notifyObservers();
-        } catch (error) {
-            console.log(error);
+        const likedJoysCopy = { ...this.likedJoys };
+        const index = likedJoysCopy.memes.findIndex(
+            (meme) => meme.url === likedMeme.url
+        );
+        if (index !== -1) {
+            likedJoysCopy.memes.splice(index, 1);
+        } else {
+            likedJoysCopy.memes.push(likedMeme);
         }
+        this.setLikedJoys(likedJoysCopy);
+        this.currentJoy = likedMeme;
+        this.notifyObservers();
     }
 
     async likeOrUnlikeJoke(likedJoke: JokeType) {
-        try {
-            const likedJoysCopy = { ...this.likedJoys };
-            const joke = await getJoy('apiId', likedJoke.apiId, 'joke');
-            if (joke.exists) {
-                const jokeToUpdate: JoyToUpdateType = {
-                    id: joke.id,
-                    type: 'joke',
-                    searchParamValue: likedJoke.apiId
-                };
-                patchLike(jokeToUpdate);
-                const index = likedJoysCopy.jokes.findIndex(
-                    (j) => j.apiId === likedJoke.apiId
-                );
-
-                if (index !== -1) {
-                    likedJoysCopy.jokes.splice(index, 1);
-                } else {
-                    likedJoysCopy.jokes.push(likedJoke);
-                }
-            } else {
-                postLike(likedJoke);
-                likedJoysCopy.jokes.push(likedJoke);
-            }
-
-            this.setLikedJoys(likedJoysCopy);
-            this.notifyObservers();
-        } catch (error) {
-            console.log(error);
+        const likedJoysCopy = { ...this.likedJoys };
+        const index = likedJoysCopy.jokes.findIndex(
+            (j) => j.apiId === likedJoke.apiId
+        );
+        if (index !== -1) {
+            likedJoysCopy.jokes.splice(index, 1);
+        } else {
+            likedJoysCopy.jokes.push(likedJoke);
         }
+        this.setLikedJoys(likedJoysCopy);
+        this.currentJoy = likedJoke;
+        this.notifyObservers();
     }
 
     async likeOrUnlikeNews(likedNews: NewsType) {
-        try {
-            const likedJoysCopy = { ...this.likedJoys };
-            const encodedUrl = encodeURIComponent(likedNews.url);
-            const news = await getJoy('url', encodedUrl, 'news');
-            if (news.exists) {
-                const newsToUpdate: JoyToUpdateType = {
-                    id: news.id,
-                    type: 'news',
-                    searchParamValue: likedNews.url
-                };
-                patchLike(newsToUpdate);
-                const index = likedJoysCopy.news.findIndex(
-                    (news) => news.url === likedNews.url
-                );
-
-                if (index !== -1) {
-                    likedJoysCopy.news.splice(index, 1);
-                } else {
-                    likedJoysCopy.news.push(likedNews);
-                }
-            } else {
-                postLike(likedNews);
-                likedJoysCopy.news.push(likedNews);
-            }
-            this.setLikedJoys(likedJoysCopy);
-            this.notifyObservers();
-        } catch (error) {
-            console.log(error);
+        const likedJoysCopy = { ...this.likedJoys };
+        const index = likedJoysCopy.news.findIndex(
+            (news) => news.url === likedNews.url
+        );
+        if (index !== -1) {
+            likedJoysCopy.news.splice(index, 1);
+        } else {
+            likedJoysCopy.news.push(likedNews);
         }
+        this.setLikedJoys(likedJoysCopy);
+        this.currentJoy = likedNews;
+        this.notifyObservers();
     }
 
     addObserver(callback: () => void) {
--- client/src/persistModel.ts
+++ client/src/persistModel.ts
@@ -1,28 +1,65 @@
 import { patchLike } from './api/patchLike';
 import { getJoy } from './api/getJoy';
 import { postLike } from './api/postLike';
-import { JoyToUpdateType } from './Types';
+import { JoyToUpdateType, MemeType, JokeType, NewsType } from './Types';
 import { CheerModel } from './models/model';
 
 export function updateServerWithModel(model: CheerModel) {
     model.addObserver(async function () {
-        const currentMeme = model.getCurrentMeme();
-        if (currentMeme) {
-            const encodedUrl = encodeURIComponent(currentMeme.url);
-            const meme = await getJoy('url', encodedUrl, 'meme');
-            if (meme.exists) {
-                const memeToUpdate: JoyToUpdateType = {
-                    id: meme.id,
-                    type: 'meme',
-                    searchParamValue: currentMeme.url
-                };
-                patchLike(memeToUpdate);
+        try {
+            const currentJoy = model.getCurrentJoy();
+            if (currentJoy?.type === 'meme') {
+                const encodedUrl = encodeURIComponent(
+                    (currentJoy as MemeType).url
+                );
+                const meme = await getJoy('url', encodedUrl, 'meme');
+                if (meme.exists) {
+                    const memeToUpdate: JoyToUpdateType = {
+                        id: meme.id,
+                        type: 'meme',
+                        searchParamValue: (currentJoy as MemeType).url
+                    };
+                    patchLike(memeToUpdate);
+                } else {
+                    postLike(currentJoy as MemeType);
+                }
+                model.resetCurrentJoy();
+            } else if (currentJoy?.type === 'joke') {
+                const joke = await getJoy(
+                    'apiId',
+                    (currentJoy as JokeType).apiId,
+                    'joke'
+                );
+                if (joke.exists) {
+                    const jokeToUpdate: JoyToUpdateType = {
+                        id: joke.id,
+                        type: 'joke',
+                        searchParamValue: (currentJoy as JokeType).apiId
+                    };
+                    patchLike(jokeToUpdate);
+                } else {
+                    postLike(currentJoy as JokeType);
+                }
+            } else if (currentJoy?.type === 'news') {
+                const encodedUrl = encodeURIComponent(
+                    (currentJoy as NewsType).url
+                );
+                const news = await getJoy('url', encodedUrl, 'news');
+                if (news.exists) {
+                    const newsToUpdate: JoyToUpdateType = {
+                        id: news.id,
+                        type: 'news',
+                        searchParamValue: (currentJoy as NewsType).url
+                    };
+                    patchLike(newsToUpdate);
+                } else {
+                    postLike(currentJoy as NewsType);
+                }
             } else {
-                postLike(currentMeme);
+                return;
             }
-            model.resetCurrentMeme();
-        } else {
-            return;
+        } catch (error) {
+            console.log(error);
         }
     });
 }
