6f7e2d8cd848511901f95ed30ec219d1c60b442c
NathalieLock <144237311+NathalieLock@users.noreply.github.com>
2023-10-23 10:25:21 +0200

    Refractor promiseNoData and usePromise (cleaner)

--- client/src/PromiseNoData.tsx
+++ client/src/PromiseNoData.tsx
@@ -1,33 +1,6 @@
-import {
-    ActivityType,
-    NewsType,
-    MemeType,
-    JokeType,
-    DataBaseType,
-    MessageType
-} from './Types';
-
-function promiseNoData(
-    promise: Promise<
-        | string
-        | MemeType[]
-        | NewsType[]
-        | JokeType
-        | ActivityType
-        | DataBaseType[]
-        | MessageType
-        | null
-    > | null,
-    data:
-        | string
-        | object
-        | MemeType[]
-        | NewsType[]
-        | JokeType
-        | ActivityType
-        | DataBaseType[]
-        | MessageType
-        | null,
+function promiseNoData<T>(
+    promise: Promise<T | null> | null,
+    data: T | null,
     error: Error | null,
     noDataMessage: string,
     bgc: string,
--- client/src/hooks/usePromise.ts
+++ client/src/hooks/usePromise.ts
@@ -8,36 +8,8 @@ import {
     MessageType
 } from '../Types';
 
-function usePromise(
-    promise: Promise<
-        | ActivityType
-        | NewsType[]
-        | MemeType[]
-        | JokeType
-        | DataBaseType[]
-        | MessageType
-    > | null
-): [
-    (
-        | ActivityType
-        | NewsType[]
-        | MemeType[]
-        | JokeType
-        | DataBaseType[]
-        | MessageType
-        | null
-    ),
-    Error | null
-] {
-    const [data, setData] = useState<
-        | ActivityType
-        | NewsType[]
-        | MemeType[]
-        | JokeType
-        | DataBaseType[]
-        | MessageType
-        | null
-    >(null);
+function usePromise<T>(promise: Promise<T> | null): [T | null, Error | null] {
+    const [data, setData] = useState<T | null>(null);
     const [error, setError] = useState<Error | null>(null);
     useEffect(
         function () {
@@ -46,19 +18,9 @@ function usePromise(
             let cancelled = false;
             if (promise)
                 promise
-                    .then(
-                        (
-                            dt:
-                                | ActivityType
-                                | NewsType[]
-                                | MemeType[]
-                                | JokeType
-                                | DataBaseType[]
-                                | MessageType
-                        ) => {
-                            if (!cancelled) setData(dt);
-                        }
-                    )
+                    .then((dt: T) => {
+                        if (!cancelled) setData(dt);
+                    })
                     .catch((er: Error) => {
                         if (!cancelled) setError(er);
                     });
