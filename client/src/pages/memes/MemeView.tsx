96eb89f93b7a6edc9330f6c48f1592b4d7478164
Frida J <97102212+frjda@users.noreply.github.com>
2023-10-23 13:18:18 +0200

    Add margin

--- client/src/pages/memes/MemeView.tsx
+++ client/src/pages/memes/MemeView.tsx
@@ -50,7 +50,7 @@ function MemeView({
 
     return (
         <div
-            className="bg-gradient-to-r from-rose-300 to-orange-300 text-black min-h-screen bg-fixed
+            className="bg-gradient-to-r from-rose-300 to-orange-300 text-black min-h-screen bg-fixed 
 dark:from-[#0d3b40] dark:to-[#0a2d30] dark:text-white"
         >
             <h1 className=" absolute top-[20%] text-4xl font-bold left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center w-144">
@@ -66,7 +66,7 @@ dark:from-[#0d3b40] dark:to-[#0a2d30] dark:text-white"
                 </button>
             </div>
             <div
-                className="absolute top-[35%] w-full bg-gradient-to-r from-rose-300 to-orange-300 text-black
+                className="absolute top-[40%] w-full bg-gradient-to-r from-rose-300 to-orange-300 text-black
 dark:from-[#0d3b40] dark:to-[#0a2d30]"
             >
                 <section className="w-full !scroll-smooth">
